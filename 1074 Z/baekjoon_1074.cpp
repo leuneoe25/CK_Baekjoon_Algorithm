fc73a284ac056c00d48d1f1987f36aa9a398e86f
네라 <leuneoe97@gmail.com>
2024-03-17 13:45:15 +0900

    25305 커트라인

---
+++ 1074 Z/baekjoon_1074.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+void recursion()
+{
+
+}
+
+int main()
+{
+
+}
---
+++ 15305 커트라인/baekjoon_25305.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool compare(int n1, int n2)
+{
+	return n1 > n2;
+}
+
+int main()
+{
+	int n, k;
+	vector<int> vect;
+
+	cin >> n >> k;
+
+	for (int i = 0; i < n; i++)
+	{
+		int num;
+		cin >> num;
+
+		vect.push_back(num);
+	}
+
+	sort(vect.begin(), vect.end(), compare);
+
+	cout << vect[k-1];
+}
