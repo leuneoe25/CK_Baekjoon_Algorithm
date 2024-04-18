16268fe80dbc2a69d03ff5e175f45843c8b71758
네라 <leuneoe97@gmail.com>
2024-04-18 11:40:40 +0900

    1436

---
+++ 1436 영화감독 숌/baekjoon_1436.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool checkNumder(int num)
+{
+	string str = to_string(num);
+	int count = 0;
+
+	for (int i = 0; i < str.length(); i++)
+	{
+		if (count >= 3)
+			return true;
+
+		if (str[i] == '6')
+			count++;
+		else
+			count = 0;
+	}
+
+	if (count >= 3)
+		return true;
+
+	return false;
+}
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	int numCount = 0;
+	int num = 0;
+
+	//cout << checkNumder(666)<<endl;
+
+	while (numCount < N)
+	{
+		num++;
+		if (checkNumder(num))
+			numCount++;
+
+	}
+
+	cout << num;
+}
